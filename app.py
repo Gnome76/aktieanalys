3a40c4a518dae22df1a7afb288371bc26d754590
Gnome76 <magber542@gmail.com>
2025-06-14 06:45:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def ta_bort_bolag(namn):
 def berakna_targetkurs(pe_vardena, ps_vardena, vinst_arsprognos, vinst_nastaar, nuvarande_kurs):
     genomsnitt_pe = sum(pe_vardena) / len(pe_vardena)
     genomsnitt_ps = sum(ps_vardena) / len(ps_vardena)
-    
+
     target_pe_ars = genomsnitt_pe * vinst_arsprognos if vinst_arsprognos and genomsnitt_pe else None
     target_pe_nastaar = genomsnitt_pe * vinst_nastaar if vinst_nastaar and genomsnitt_pe else None
 
@@ -80,7 +80,6 @@ def berakna_targetkurs(pe_vardena, ps_vardena, vinst_arsprognos, vinst_nastaar, 
         undervardering_ars = (target_pe_ars / nuvarande_kurs) - 1
     if nuvarande_kurs and target_pe_nastaar:
         undervardering_nastaar = (target_pe_nastaar / nuvarande_kurs) - 1
-
     if nuvarande_kurs and target_genomsnitt_ars:
         undervardering_genomsnitt_ars = (target_genomsnitt_ars / nuvarande_kurs) - 1
     if nuvarande_kurs and target_genomsnitt_nastaar:
@@ -98,7 +97,6 @@ def berakna_targetkurs(pe_vardena, ps_vardena, vinst_arsprognos, vinst_nastaar, 
         "undervardering_genomsnitt_ars": undervardering_genomsnitt_ars,
         "undervardering_genomsnitt_nastaar": undervardering_genomsnitt_nastaar,
     }
-
 
 def main():
     st.title("Aktieinnehav - Spara och analysera")
@@ -169,7 +167,6 @@ def main():
             target_lista.append(resultat)
 
         df_target = pd.DataFrame(target_lista)
-
         df_display = pd.concat([df.reset_index(drop=True), df_target], axis=1)
 
         st.subheader("Alla sparade bolag")
@@ -193,4 +190,5 @@ def main():
             "target_ps_ars": "{:.2f}",
             "target_ps_nastaar": "{:.2f}",
             "target_genomsnitt_ars": "{:.2f}",
-            "target_gen
+            "target_genomsnitt_nastaar": "{:.2f}",
+            "undervardering_pe
